3d34029c05772fe1cc37d7a33ddb457a20bae696
tsoglani <mrtsoglanakos@hotmail.com>
2016-09-07 22:43:09 +0300

    Final commit

--- PokemonWatchFace/mobile/src/androidTest/java/tsoglanakos/pokemon/watch_face/pika/ApplicationTest.java
+++ PokemonWatchFace/mobile/src/androidTest/java/tsoglanakos/pokemon/watch_face/pika/ApplicationTest.java
@@ -1,4 +1,4 @@
-package tsoglani.pokemon.watch_face.pokemonwatchface;
+package tsoglanakos.pokemon.watch_face.pika;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- PokemonWatchFace/wear/src/main/java/tsoglanakos/pokemon/watch_face/pika/MyWatchFace.java
+++ PokemonWatchFace/wear/src/main/java/tsoglanakos/pokemon/watch_face/pika/MyWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package tsoglani.pokemon.watch_face.pokemonwatchface;
+package tsoglanakos.pokemon.watch_face.pika;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
